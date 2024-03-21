42ad892587a4e79a2ad8e344e5d147a7024533c8
vincentdang92 <vincentdang92@gmail.com>
2024-03-21 18:02:42 +0700

    task init

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index( Request $request){
+        return new TaskCollection(Task::all());
+    }
+    public function show(Request $request, Task $task){
+        return new TaskResource($task);
+    }
+    public function store(StoreTaskRequest $request){
+        $validate = $request->validate();
+        $task = Task::create($validate);
+        return new TaskResource($task);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title'
+    ];
+    protected $casts = [
+        'is_done'=> 'boolean'
+    ];
+    protected $hidden = [
+        'updated_at'
+    ];
 }
